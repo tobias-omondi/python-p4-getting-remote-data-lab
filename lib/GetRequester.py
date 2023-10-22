9e04e20063cc3fc978123f22c6052f69927938ef
tobias <tobias.omondi@student.moringaschool.com>
2023-10-22 14:50:53 +0300

    updated

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        r = requests.get(URL)
+        return r.content
 
     def load_json(self):
-        pass
+
+        programs = json.loads(self.get_response_body())
+      
+        return programs
+
+       
